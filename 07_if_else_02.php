95841154201871561994d65bb0ad5eaaaa7cb889
andredeveloperPHP <85752918+andredeveloperPHP@users.noreply.github.com>
2021-06-14 00:21:44 -0300

    update

--- 07_if_else_02.php
+++ 07_if_else_02.php
@@ -8,22 +8,22 @@ $idade = 15;
 $numero_de_pessoas = 2;
 $signo = "Libra";
 
-echo "Voce pode entrar na festa se tiver apartir de 18 anos ou ";
-echo "A partir de 16 anos acompanhado <br>";
+echo ucwords("Voce pode entrar na festa se tiver apartir de 18 anos ou ");
+echo ucwords("A partir de 16 anos acompanhado <br>");
 
 if($idade >= 18){
-    echo "Você tem a ".$idade." Você pode entrar na festa tranquilamente!! <br>";
+    echo ucwords("Você tem a ").$idade." Você pode entrar na festa tranquilamente!! <br>";
 }else{
     if($idade >= 16 && $numero_de_pessoas > 1){
-        echo " \t Voce pode entrar pois tem uma pessoa de acompanhante: " . "<br>";
+        echo ucwords(" \t Voce pode entrar pois tem uma pessoa de acompanhante: " . "<br>");
         echo "<br>";
     }
         else if($idade >=15 && $numero_de_pessoas > 1 && $signo == "Libra"){
-        echo "Voce tem todas as condições para entrar: <br> " . "Signo:" . "{$signo}"."Esta acompalhado com pelo menos 1 Pessoa: <br> "."QTDE Pessoas: " . "{ $numero_de_pessoas}" . "Tem idade suficiente: <br>" . "{$idade}" . "<br>";
+        echo ucwords("Voce tem todas as condições para entrar: <br> ") . "Signo:" . "{$signo}"."Esta acompalhado com pelo menos 1 Pessoa: <br> "."QTDE Pessoas: " . "{ $numero_de_pessoas}" . "Tem idade suficiente: <br>" . "{$idade}" . "<br>";
    
    }else{
 
-   echo " \t Você so tem a idade de: " .$idade. " e Não pode entrar nem Fodendo!! <br> ";
+   echo ucwords(" \t Você so tem a idade de: ") .$idade. " e Não pode entrar nem Fodendo!! <br> ";
    }
 }
 
