6c5edac70c3e036f3dff295a140b12434766228e
andredeveloperPHP <85752918+andredeveloperPHP@users.noreply.github.com>
2021-06-14 00:20:01 -0300

    Update 06_if_else.php
    atualização

--- 06_if_else.php
+++ 06_if_else.php
@@ -7,16 +7,16 @@ $idade = 16;
 echo "<br>";
 
 if($idade >= 16){
-    echo "Voce tem ".$idade." anos ".PHP_EOL;
-    echo "Pode Entrar sozinho";
+    echo ucwords("Voce tem ").$idade." anos ".PHP_EOL;
+    echo ucwords("Pode Entrar sozinho");
 }
 
 else if($idade >= 18){
-    echo "Você so tem ".$idade." anos. Você não pode entrar sozinho \n";
+    echo ucwords("Você so tem ").$idade." anos. Você não pode entrar sozinho \n";
     echo "<br>";
 }
 else{
-    echo "Voce não pode entrar na Balada <br>";
+    echo ucwords("Voce não pode entrar na Balada <br>");
     echo "\n Adeus ";
 
 }
