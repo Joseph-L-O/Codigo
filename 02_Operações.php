ef56dd66a0ce98a884464399c7b106418dd03656
andredeveloperPHP <85752918+andredeveloperPHP@users.noreply.github.com>
2021-06-14 00:13:36 -0300

    update de operações basicas matematicas
    atualização

--- 02_Operações.php
+++ 02_Operações.php
@@ -8,50 +8,54 @@ $idade = 21;
 //minha idade daqui 10 anos:
 
 $minhaidade_daqui10anos = $idade + 10;
-echo "Minha idade daqui dez anos será de: <br>" . $minhaidade_daqui10anos."<br>";
+echo ucwords("Minha idade daqui dez anos será de: <br>") . $minhaidade_daqui10anos."<br>";
 echo "<br>";$
 
-//Minha Idade subtraida:
+/*
+*@param Subtração de valores:
+*/
 $minhaidade_daqui5anos = $idade - 5;
 
-echo "Minha idade com menos 5 anos será de: <br>" . $minhaidade_daqui5anos."<br>";
+echo ucwords("Minha idade com menos 5 anos será de: <br>") . $minhaidade_daqui5anos."<br>";
 echo "<br>";
 
 
 //Multiplicação:
 $minhaidademultiplicada = $idade * 2;
-echo "Minha idade multiplicada no multiplicador comum <br>". $minhaidademultiplicada . "<br>";
+echo strtoupper("Minha idade multiplicada no multiplicador comum <br>"). $minhaidademultiplicada . "<br>";
 echo "<br>";
 
 //Divisão da Idade:
 $minhaidadeDivisão = $idade / 2;
-echo "Minha idade Dividida por 2 será de: <br>". $minhaidadeDivisão . "<br>";
+echo ucwords("Minha idade Dividida por 2 será de: <br>"). $minhaidadeDivisao . "<br>";
 echo "<br>";
-// aqui temos as quatro operações matematicas para trabalhar com o PHP
-// existem outros operadores <matematicos>
-//Potencia:
+/*
+*@param aqui temos as quatro operações matematicas para trabalhar com o PHP
+*@param existem outros operadores <matematicos>
+*@param Potencia:
+*/ 
 $dois_ao_cubo = 2 * 2 * 2;
-echo "A potencia elevada ao cubo do valor de dois sera: <br>". $dois_ao_cubo . "<br>";
+echo ucwords("A potencia elevada ao cubo do valor de dois sera: <br>"). $dois_ao_cubo . "<br>";
 echo "<br>";
 
 // A outras formas de elevar um numero ao cubo:
 $valor = 30;
 $resultado = $valor**2;
 
-echo "A variavel com o Valor de: { $valor } <br> Sera elevada ao Cubo com a função: <br>";
+echo ucwords("A variavel com o Valor de: { $valor } <br> Sera elevada ao Cubo com a função: <br>");
 echo "<br>";
-echo "Esse e o Resultado: <br>". $resultado . "<br>";
+echo ucwords("Esse e o Resultado: <br>"). $resultado . "<br>";
 echo "<br>";
 
-// Pegando o resto da Divisão:
-
+/*
+*@param Fim dos exemplos de funções matematicas 
+*
+*/
 $restodadivisao = 10 % 3;
-echo "O resto da Divisão Sera: <br>" . $restodadivisao . "<br>";
+echo ucwords("O resto da Divisão Sera: <br>").$restodadivisao . "<br>";
 echo "<br>";
 
-// fim dos exemplos e tipos de operações matematicas.
-
-
-
-
+/*
+*@param Fim dos exemplos de funções Matematicas;
+*/
 ?>
