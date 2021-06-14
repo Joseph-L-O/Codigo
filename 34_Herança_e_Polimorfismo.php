8c9d717dbb426d23490a890da5280c7335fafa0d
Andre Santos <andre.santos@1342@yahoo.com>
2021-06-13 21:37:48 -0300

    update files

--- 34_Herança_e_Polimorfismo.php
+++ 34_Herança_e_Polimorfismo.php
@@ -41,7 +41,7 @@ public function __Construct(string $cidade, string $bairro , string $rua , strin
     }
 }
 
-class Conta {
+class Conta{
     private $saldo;
     private $cpfTitular;
     private $nomeTitular;
@@ -51,8 +51,36 @@ class Conta {
     {
         $this->saldo = 0;
         $this->cpfTitular = $cpfdoTitular;
+        self::validanome($nomedoTitular);
         $this->nomeTitular = $nomedoTitular;
         $this->numerodecontas++;
 
+    }
+    /**
+     * @param Metodos da classe conta:
+     */
+
+    public function recuperasaldo():float
+    {
+        return $this->saldo;
+    }
+    public function recuperacpf():float 
+    {
+        return $this->cpfTitular;
+    }
+    public function recuperanome():float 
+    {
+        return $this->nomeTitular;
+    }
+
+    public static function validarnome(string $nomedoTitular):string
+    {
+        if(strlen($nomedoTitular) < 5)
+        {
+             throw New Exception("o nome deve ter mais que 05 caracteres");
+        }
+        else{
+            exit();
+        }
     }
 }
