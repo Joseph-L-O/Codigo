9e39969b92c61f51f656a3cb3b2fd1a0a5cd0dca
andredeveloperPHP <85752918+andredeveloperPHP@users.noreply.github.com>
2021-06-14 00:16:50 -0300

    update de strings em PHP
    atiualização strings

--- 04_Strings.php
+++ 04_Strings.php
@@ -3,9 +3,9 @@
  * @param strings em php
  * @param formatações de strings em php
  *  */
-echo "Ola Mundo";
+echo ucwords("Ola Mundo");
 $idade = 21;
-echo 'Minha idade e: ' . $idade . '<br>';
+echo ucwords('Minha idade e: ') . $idade . '<br>';
 echo "<br>";
 echo " \t Ola mundo novamente: \n ";
 echo "<br>";
