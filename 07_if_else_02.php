63961433315cc405cfbfeaebaf9d8ccf01b81e43
andredeveloperPHP <85752918+andredeveloperPHP@users.noreply.github.com>
2021-06-15 00:10:23 -0300

    changes avaiable
    update codigo

--- 07_if_else_02.php
+++ 07_if_else_02.php
@@ -1,13 +1,11 @@
 <?php
 /**
  * @param Exemplo com if e else
+ * @param Varaveis declaradas a seguir:
  */
-
-// variaveis declaradas:
 $idade = 15;
 $numero_de_pessoas = 2;
 $signo = "Libra";
-
 echo ucwords("Voce pode entrar na festa se tiver apartir de 18 anos ou ");
 echo ucwords("A partir de 16 anos acompanhado <br>");
 
@@ -20,12 +18,8 @@ if($idade >= 18){
     }
         else if($idade >=15 && $numero_de_pessoas > 1 && $signo == "Libra"){
         echo ucwords("Voce tem todas as condições para entrar: <br> ") . "Signo:" . "{$signo}"."Esta acompalhado com pelo menos 1 Pessoa: <br> "."QTDE Pessoas: " . "{ $numero_de_pessoas}" . "Tem idade suficiente: <br>" . "{$idade}" . "<br>";
-   
    }else{
-
    echo ucwords(" \t Você so tem a idade de: ") .$idade. " e Não pode entrar nem Fodendo!! <br> ";
    }
 }
-
-
 ?>
