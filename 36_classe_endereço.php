23641c765bad52b5cb3063cd31dd78cfb53c2166
Andre Santos <andre.santos@1342@yahoo.com>
2021-06-14 21:28:43 -0300

    update to files

--- 36_classe_endereço.php
+++ 36_classe_endereço.php
@@ -1,127 +1,124 @@
 <?php
 /**
- * @param Criação das classes Conta, e Endereco;
- * @param Validar nome e um metodo statico
- * @param $numerodecontase um atributo Estatico;
+ * @param primeiro vou criar a classe conta;
+ * @param atributos setados
  */
-class Conta {
-  private $saldo;
-  private $cpfTitular;
-  private $nomeTitular;
-  public static $numerodecontas;
-   
-  /**
-   * @param Adicionando um Construtor();
-   * @param inicializando o Construtor;
-   */
-  public function __construct(string $cpfdoTitular, string $nomedoTitular)
-  {
-    $this->saldo = 0;
-    $this->cpfTitular = $cpfdoTitular;
-    self::validarnome($nomedoTitular);
-    $this->nomeTitular = $nomedoTitular;
-    $this->numerodecontas++;
-  }
 
+ class Conta{
+   private $saldo;
+   private $cpfTitular;
+   private $nomeTitular;
+   public static $numerodecontas;
 
-  /**
-   * @param Setando todos os Metodos
-   */
-  public function recuperasaldo():float
-    {
-        return $this->saldo;
-    }
-    public function recuperacpf():float 
-    {
-        return $this->cpfTitular;
-    }
-    public function recuperanome():float 
-    {
-        return $this->nomeTitular;
-    }
-    public function sacar(float $valorAsacar):float 
-    {
-        if($valorAsacar > $this->saldo)
-        {
-            throw New Exception("Não e Possivel Sacar o valor!!");
-        }else if($valorAsacar == 0)
-        {
-            throw New Exception("O valor a Sacar não pode ser o Numero 0");
-        }else{
-            $valorAsacar -=$this->saldo;
-        }
-    }
-    public function depositar(float $deposito):float 
-    {
-        if($deposito == 0)
-        {
-            throw New Exception("O deposito Precisa possuir um valor e não podera ser zero!!");
+   public function __Construct(float $saldoconta, string $cpfdoTitular, string $nomedoTitular)
+   {
+       $this->saldo = $saldoconta;
+       $this->cpfTitular = $cpfdoTitular;
+       $this->nomeTitular = $nomedoTitular;
+       
+       Conta::$numerodecontas++;
+   }
+   public function recuperarsaldo():float
+   {
+       return $this->saldo;
+   }
+   public function recuperarnome():string 
+   {
+       return $this->nomeTitular;
+   }
+   public function recuperarcpf():string 
+   {
+       return $this->cpfTitular;
+   }
+   public function sacar(float $valor)
+   {
+       if($valor > $this->saldo)
+       {
+           throw New Exception("Não e Possivel sacar o valor!");
+       }
+       else if($valor == 0)
+       {
+           throw New Exception("Não e Possivel sacar valor Zerado!!");
+       }
+       else {
+           $valor -= $this->saldo;
+       }
+   }
+   public function depositar(float $deposito)
+   {
+       if($deposito < 0)
+       {
+           throw New Exception("O valor não pode ser Negativo!");
+       }
+       else if($deposito == 0)
+       {
+           throw New Exception("O valor não pode ser Zero");
+       }
+       else{
+           $this->saldo += $deposito;
+       }
+   }
+   public function transferir(float $valor, Conta $ContaDestino)
+   {
+       if($valor > $this->saldo)
+       {
+           throw New Exception("Não e Possivel Transferir o valor");
+       }
+       else if($valor == 0)
+       {
+           throw New Exception("O valor da transferencia não pode ser igual a: 0");
+       }
+       else{
+           $this->sacar = $valor;
+           $ContaDestino->depositar($ContaDestino);
+       }
+   }
 
-        }else if($deposito < 0 )
-        {
-            throw New Exception("O Numero Preisa ser Positivo!!");
-
-        }else{
-            $deposito += $this->saldo;
-        }
-    }
-        public function transferir(float $valor, Conta $contaDestino)
-    {
-        if($valor > $this->saldo)
-        {
-            throw New Exception("Não e Possivel transferir o Valor");
-
-        }else{
-            $this->sacar($valor);
-            $contaDestino = $this->depositar($contaDestino);
-        }
-    }
-    public static function validarnome(string $nomedoTitular):string
-    {
-        if(strlen($nomedoTitular) < 5)
-        {
-             throw New Exception("o nome deve ter mais que 05 caracteres");
-        }else{
-            exit();
-        }
-    }
-}    
-
-/**
- *@param Setando uma Nova Classe
- */
-class Endereco {
-  private $cidade;
-  private $bairro;
-  private $rua;
-  private $numero;
-  /**
-   * @param Construtor Inicializado;
-   */
-  public function __construct(string $cidade , string $bairro , string $rua , string $numero){
-    $this->cidade = $cidade;
-    $this->bairro = $bairro;
-    $this->rua = $rua;
-    $this->numero = $numero;
-  }
-  public function recuperacidade():string
-  {
-    return $this->cidade;
-  }
-  public function recuperabairro():string
-  {
-    return $this->bairro;
-  }
-  public function recuperarua():string
-  {
-    return $this->rua;
-  }
-  public function recuperarnumero():string
-  {
-    return $this->numero;
-  }
-  
 }
 
+ class Endereco 
+{
+    private $cidade;
+    private $bairro;
+    private $rua;
+    private $numero;
+
+    public function __Construct(string $cidade, string $bairro, string $rua, string $numero)
+    {
+        $this->cidade = $cidade;
+        $this->bairro = $bairro;
+        $this->rua    = $rua;
+        $this->numero = $numero;
+    }
+
+    public function recuperarcidade():string 
+    {
+        return $this->cidade;
+    }
+    public function recuperarbairro():string 
+    {
+        return $this->bairro;
+    }
+    public function recuperarrua():string 
+    {
+        return $this->rua;
+    }
+    public function recuperarnumero():string 
+    {
+        return $this->numero;
+    }
+}
+
+/**
+ * @param Instanciando classe
+ */
+
+ $primeiraconta = new Conta("100", "237.216.498.78", "Andre Panizza dos Santos");
+ echo ucwords("O saldo da conta R$:  ").$primeiraconta->recuperarsaldo()."<br>";
+ echo ucwords("Nome do Titular da Conta: ").$primeiraconta->recuperarnome()."<br>";
+ echo ucwords("Numero do CPF do Titular: ").$primeiraconta->recuperarcpf()."<br>";
+ echo "<hr>";
+ 
+ 
 
 ?>
