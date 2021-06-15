bfd903cede9ad9940b1b71a76708ffdf2a14ec35
andredeveloperPHP <85752918+andredeveloperPHP@users.noreply.github.com>
2021-06-15 00:23:35 -0300

    Loop
    Loop update

--- 14_exemplo_Loop.php
+++ 14_exemplo_Loop.php
@@ -2,25 +2,20 @@
 /**
  * @param Exemplo de um laço de repetição
  */
-
 function Lista(){
     //Lista de array:
     $Notas = [6.0, 5.0, 7.0, 4.6, 5.9, 7.7];
     $Nomes_Alunos = ["Andre", "Stephanie", "Roseli", "Alcides","Aurea", "toto"];
-    $frase = ucfirst("Você foi Aprovado!!  <br>");
+    $frase = ucfirst("Você foi Aprovado!!<br>");
     /**
      *@param Exemplo de Loop usando o laço for
      */
-         
     for($i = 0; $i < count($Notas); $i++){
-        echo "<br>".$Notas[$i] . $Nomes_Alunos[$x];
+    echo "<br>".$Notas[$i] . $Nomes_Alunos[$x];
     }
-
 }
 /**
  * @param exibindo o valor do array;
  */
 echo Lista() . "<br>";
-
-
 ?>
