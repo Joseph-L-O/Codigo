161206b4e5ebf13fc7f367d7866d4219316a7433
andredeveloperPHP <85752918+andredeveloperPHP@users.noreply.github.com>
2021-06-15 00:13:09 -0300

    atualização contador
    atualização contador

--- 10_repeticao.php
+++ 10_repeticao.php
@@ -1,20 +1,14 @@
 <?php
 /**
  * @param Loop de repetição While;
+ * @param iniciando um contador;
+ * @param enquanto o contador não for igual a 15 faça:
+ * @param Escreva na tela;
  */
 $contador = 0;
-
-//enquanto esse numero não for maior ou igual a 15 exibo o numero quero a linha e adiciono mais um e repito:
-    
-//enquanto não chegar a 15 mostre esse codigo:
-
 while($contador < 15){
 echo "#$contador" . "<br>";
 // e o contador, recebe o valor do contador + 1;
-
-
 $contador = $contador + 1;
-
 }
-
 ?>
