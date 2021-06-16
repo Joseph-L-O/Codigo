349ca32ea506bf05bb48f13930488a27c02e2052
andredeveloperPHP <85752918+andredeveloperPHP@users.noreply.github.com>
2021-06-16 15:34:35 -0300

    Update CustomDAO.php

--- AKZO/COBRANÇA/CustomDAO.php
+++ AKZO/COBRANÇA/CustomDAO.php
@@ -8,22 +8,16 @@ class CustomDAO
         $this->con->debug = false;
         $this->logger = logger::getInstance();
     }
-
     public function begin() {
         $this->con->BeginTrans();
     }
-
     public function commit() {
         $this->con->CommitTrans();
     }
-
     public function rollback() {
         $this->con->RollbackTrans();
     }
-
-
     public function consultaCNPJ($cnpj) {
-
         $sql = "SELECT 
                     cnpj
                 FROM
@@ -39,7 +33,6 @@ class CustomDAO
             return false;
          }
     }
-
     public function insereRegistros($reference, $headerArquivo, $headerLote, $segmentoT, $segmentoU, $traillerLote, $traillerArquivo) {
 
     $sql = "INSERT INTO santander_confirming_cobrancas (reference, header_arquivo, header_lote, segmento_t, segmento_u, trailler_lote, trailler_arquivo) VALUES (?, ?, ?, ?, ?, ?, ?)";
