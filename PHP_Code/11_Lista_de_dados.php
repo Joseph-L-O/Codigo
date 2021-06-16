9ed58b5cbcc31653063bbd4b4f88d304f4a66639
Andre Santos <andre.santos@1342@yahoo.com>
2021-06-16 14:06:13 -0300

    update to files

--- PHP_Code/11_Lista_de_dados.php
+++ PHP_Code/11_Lista_de_dados.php
@@ -4,16 +4,11 @@
  * @param criando um array.
  */ 
 // array com chave e valor com indice:
-$variavel = ["123.456.432.32 => ['Conta' => 'Andre Panizza dos Santos', 'Saldo' => 100],
-                                ['Conta' => 'Patricia dos Santos, 'Saldo' => 200],
-                                ['Conta' => 'Raul dos Santos Lopes', 'Saldo' => 300],
-                                ['Conta' => 'Stephanie King dos Santos', 'Saldo' => 400]
-                                     
-            ];
-/*
-*@param Variaveis e arrays declarados;
-*/
+$variavel = ["123.456.432.32" => ['Conta' => 'Andre Panizza dos Santos','Saldo' => 100],
+                                ['Conta' => 'Patricia dos Santos','Saldo' => 200],
+                                ['Conta' => 'Raul dos Santos Lopes','Saldo' => 300],
+                                ['Conta' => 'Stephanie King dos Santos', 'Saldo' => 400],
+];
 
+#@param Variaveis e arrays declarados;
 
-
-?>
