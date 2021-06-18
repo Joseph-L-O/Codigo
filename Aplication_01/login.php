d75c5a38fd60989bc884ed4b9523734da4b6b67e
Andre Santos <andre.santos@1342@yahoo.com>
2021-06-18 00:22:04 -0300

    update

---
+++ Aplication_01/login.php
@@ -0,0 +1,21 @@
+<?php
+?>
+<!Doctype html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="/Aplication_01/css/">
+<link rel="shortcut icon" href="/WebRoot/Store/Shops/......./MediaGallery/favicon.ico" type="image/x-icon"/>
+<link rel="author" href="./src/author.php">
+<script type="text/javascript" src="/Aplication_01/script/"></script>
+
+</head>
+
+
+
+
+
+
+
+
+
+</html>
