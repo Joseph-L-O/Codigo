0a97d58c7688e5b1a7fdfb09c7d494bfc4df673f
andredeveloperPHP <85752918+andredeveloperPHP@users.noreply.github.com>
2021-06-18 18:53:54 -0300

    Update script.js

--- Aplication_01/script/script.js
+++ Aplication_01/script/script.js
@@ -2,7 +2,13 @@
 /* criando uma constante pra teste e chamado de box-1 */
 
 const box1 = document.getElementById('box-1');
-const box2 = querySelectorById('box-2');
+console.log(box1);
+// exibo o box 1;
+
+const box2 = querySelector('box-2');
+console.log(box2);
+
+//exibo o box 2
 
 /* document: No meu documento HTML, busca pra mim, busca o elemento pelo ID */
 /* E o nome do elemento que eu quero que e BOX-1 */
