9905869665dede9468ea796d076c29fda4cc9b74
Andre Santos <andre.santos@1342@yahoo.com>
2021-06-18 18:57:11 -0300

    origin master

--- Aplication_01/login.php
+++ Aplication_01/login.php
@@ -3,11 +3,11 @@
 <!Doctype html>
 <head>
 <meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="/Aplication_01/css/" title="folha de estilo">
+<link rel="stylesheet" type="text/css" href="/Aplication_01/css/style.css" title="folha de estilo">
 <link rel="shortcut icon" href="/WebRoot/Store/Shops/......./MediaGallery/favicon.ico" type="image/x-icon"/>
 <link rel="author" href="./src/author.php">
 <link rel="preload"  title="auto-load-page" alt="none">
-<script type="text/javascript" src="/Aplication_01/script/"></script>
+<script type="text/javascript" src="/Aplication_01/script/script.js"></script>
 </head>
 <body id="corpo">
   <!-- testando uma div -->
