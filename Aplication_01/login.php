c33ce35862aa7defe5fbdfdf4ceb49107291fa3b
andredeveloperPHP <85752918+andredeveloperPHP@users.noreply.github.com>
2021-06-18 18:19:16 -0300

    alterações da pagina
    login.php atributos alterados

--- Aplication_01/login.php
+++ Aplication_01/login.php
@@ -3,9 +3,13 @@
 <!Doctype html>
 <head>
 <meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="/Aplication_01/css/">
+<link rel="stylesheet" type="text/css" href="/Aplication_01/css/" title="folha de estilo">
 <link rel="shortcut icon" href="/WebRoot/Store/Shops/......./MediaGallery/favicon.ico" type="image/x-icon"/>
 <link rel="author" href="./src/author.php">
+<link rel="preload"  title="auto-load-page" alt="none">
 <script type="text/javascript" src="/Aplication_01/script/"></script>
 </head>
+<body>
+inicio da pagina principal  
+<html>  
 </html>
