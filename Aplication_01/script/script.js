47a859b299e82c8ca35e9e8e36471a3878f1bc76
andredeveloperPHP <85752918+andredeveloperPHP@users.noreply.github.com>
2021-06-18 18:50:20 -0300

    Update script.js

---
+++ Aplication_01/script/script.js
@@ -0,0 +1,7 @@
+
+*/criando uma constante pra teste e chamado de box-1*/
+
+const box1 = document.getElementbyId('box-1');
+
+/*document: No meu documento HTML, busca pra mim, busca o elemento pelo ID */
+/* E o nome do elemento que eu quero que e BOX-1 */
