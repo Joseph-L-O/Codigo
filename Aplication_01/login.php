d4104da6da52a9f5fee73b18cf6c2cbbfc0623f7
Andre Santos <andre.santos@1342@yahoo.com>
2021-06-18 19:23:16 -0300

    .

---
+++ Aplication_01/login.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Título da página</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="">
+    <link rel="stylesheet"    href="">
+    <script src=""></script>
+  </head>
+  <body>
+    Aqui vai o código HTML que fará seu site aparecer.
+  </body>
+</html>
