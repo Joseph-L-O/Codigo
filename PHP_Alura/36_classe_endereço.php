f06dac090aaad9999588e4f66ee6aa46421e91d4
Andre Santos <andre.santos@1342@yahoo.com>
2021-06-19 18:16:40 -0300

    update

--- PHP_Alura/36_classe_endereço.php
+++ PHP_Alura/36_classe_endereço.php
@@ -1,2 +1,23 @@
 <?php
-?>
+/**
+ * @author  Andre Panizza dos santos <https://github.com/andredeveloperPHP>
+ * @version 0.0.-1; 
+ */
+class Conta {
+/**
+ * criação dos atributos
+ * @access private 
+ */
+private $saldo; 
+private $cpfTitular;
+private $nomeTitular;
+/**
+* @internal Construtor, que obriga a declaração das informações
+*/
+public function __Construct(string $cpfdotitular, string $nomedotitular){
+    $this->saldo = 0;
+    $this->cpfTiTular = $cpfdotitular;
+    $this->nomeTitular = $nomedotitular;
+}
+
+}
