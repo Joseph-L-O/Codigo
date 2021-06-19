7ad6962240f0c346bb5492a9151866a891dfe04a
Andre Santos <andre.santos@1342@yahoo.com>
2021-06-18 22:58:20 -0300

    update

--- Aplication_01/login.php
+++ Aplication_01/login.php
@@ -7,11 +7,25 @@
     <title>Título da página</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="">
-    <link rel="stylesheet"    href="">
-    <script src=""></script>
+    <link rel="short icon" href="./icones/Favicons/restricted_folder_file_10341 (1).ico">
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="./js/file-01.js"></script>
   </head>
-  <body>
-    Aqui vai o código HTML que fará seu site aparecer.
+  <body id="corpo-1">
+    <main>
+        <article>
+            <div id="box-1"><h2 id="title-0"> BEM VINDO </h2></div>
+            <div id="box2">
+                <h3 id="forms-1" name="titulo formulario"> Cadastro de Clientes </h3>
+
+
+
+            </div>
+        </article>
+    </main>
+    <footer>
+
+    </footer>  
+    
   </body>
 </html>
