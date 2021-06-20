1b1d0580ea1bfeb73356002360c2c9c03db17c30
andredeveloperPHP <85752918+andredeveloperPHP@users.noreply.github.com>
2021-06-20 19:46:20 -0300

    Update file-01.js

--- Aplication_01/js/file-01.js
+++ Aplication_01/js/file-01.js
@@ -1,1 +1,12 @@
 
+et frutas = "frutas";
+let foo = ["Melancia", "Manga", "Laranja", "Abacaxi"];
+
+for (let i = 0; i < 4; i++) {
+    console.log(frutas + foo[i]);
+}
+
+$frutas = array("Melancia", "Manga", "Laranja", "Abacaxi");
+for ($i = 0; $i < 4; i++) {
+    echo("frutas".$frutas[$i]);
+}
