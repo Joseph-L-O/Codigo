4befa506707838c8a28502f80b12e1122779e88b
andredeveloperPHP <85752918+andredeveloperPHP@users.noreply.github.com>
2021-06-21 11:49:54 -0300

    Update file-01.js

--- Aplication_01/js/file-01.js
+++ Aplication_01/js/file-01.js
@@ -1,11 +1,14 @@
 let frutas = "frutas";
 let foo = ["Melancia", "Manga", "Laranja", "Abacaxi"];
-
-for (let i = 0; i < 4; i++) {
-    console.log(frutas + foo[i]);
+let names = ["alexandre","magno","Gabriel","Raul","Jair Bolsonaro"];
+let Presidentes = ["Hamiltom Mourão","General Heleno","General Pazuelo"];
+let Vicegovernadores = ["Reinaldo da silva","Gael","hugo"];
+let Prefeitos = ["Marcos Neves-Carapicuiba","Eduardo Leite","Rogerio Lins-Osasco","Bruno Covas-SP"];
+let cont = 0;
+let i=0;
+for (let cont=0; cont <= 9; cont++){
+    console.log('valor' + names[cont]);
 }
-
-$frutas = array("Melancia", "Manga", "Laranja", "Abacaxi");
-for ($i = 0; $i < 4; i++) {
-    echo("frutas".$frutas[$i]);
+for (let i = 0;  i < 4; i++) {
+    console.log(frutas + foo[i]);
 }
