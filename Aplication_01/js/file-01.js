cc96a58837c04e9778ae809714be4c613878b092
joseph-l-o <superrecgt@gmail.com>
2021-06-21 17:14:49 -0300

    added footer

--- Aplication_01/js/file-01.js
+++ Aplication_01/js/file-01.js
@@ -1,14 +1,84 @@
-let frutas = "frutas";
-let foo = ["Melancia", "Manga", "Laranja", "Abacaxi"];
-let names = ["alexandre","magno","Gabriel","Raul","Jair Bolsonaro"];
-let Presidentes = ["Hamiltom Mourão","General Heleno","General Pazuelo"];
-let Vicegovernadores = ["Reinaldo da silva","Gael","hugo"];
-let Prefeitos = ["Marcos Neves-Carapicuiba","Eduardo Leite","Rogerio Lins-Osasco","Bruno Covas-SP"];
-let cont = 0;
-let i=0;
-for (let cont=0; cont <= 9; cont++){
-    console.log('valor' + names[cont]);
-}
-for (let i = 0;  i < 4; i++) {
-    console.log(frutas + foo[i]);
+// let frutas = "frutas";
+// let foo = ["Melancia", "Manga", "Laranja", "Abacaxi"];
+// let names = ["alexandre","magno","Gabriel","Raul","Jair Bolsonaro"];
+// let Presidentes = ["Hamiltom Mourão","General Heleno","General Pazuelo"];
+// let Vicegovernadores = ["Reinaldo da silva","Gael","hugo"];
+// let Prefeitos = ["Marcos Neves-Carapicuiba","Eduardo Leite","Rogerio Lins-Osasco","Bruno Covas-SP"];
+// let cont = 0;
+// let i=0;
+// for (let cont=0; cont <= 9; cont++){
+//     console.log('valor' + names[cont]);
+// }
+// for (let i = 0;  i < 4; i++) {
+//     console.log(frutas + foo[i]);
+// }
+let sub1 = document.getElementsByClassName('subcontent')
+console.log(sub1)
+let sub1r = true;
+let sub2r = true;
+let sub3r = true;
+let sub4r = true;
+
+const subcontent = (val) => {
+    if (val == 0) {
+        if (sub1r) {
+            sub1[val].style.display = 'block'
+            sub1[1].style.display = 'none'
+            sub1[2].style.display = 'none'
+            sub1[3].style.display = 'none'
+
+            sub2r = true;
+            sub3r = true;
+            sub4r = true;
+            sub1r = false
+        } else {
+            sub1[val].style.display = 'none'
+            sub1r = true;
+        }
+    }
+    if (val == 1) {
+        if (sub2r) {
+            sub1[val].style.display = 'block'
+            sub1[0].style.display = 'none'
+            sub1[2].style.display = 'none'
+            sub1[3].style.display = 'none'
+            sub1r = true;
+            sub3r = true;
+            sub4r = true;
+            sub2r = false
+        } else {
+            sub1[val].style.display = 'none'
+            sub2r = true;
+        }
+    }
+    if (val == 2) {
+        if (sub3r) {
+            sub1[val].style.display = 'block'
+            sub1[0].style.display = 'none'
+            sub1[1].style.display = 'none'
+            sub1[3].style.display = 'none'
+            sub2r = true;
+            sub1r = true;
+            sub4r = true;
+            sub3r = false
+        } else {
+            sub1[val].style.display = 'none'
+            sub3r = true;
+        }
+    }
+    if (val == 3) {
+        if (sub4r) {
+            sub1[val].style.display = 'block'
+            sub1[0].style.display = 'none'
+            sub1[1].style.display = 'none'
+            sub1[2].style.display = 'none'
+            sub2r = true;
+            sub3r = true;
+            sub1r = true;
+            sub4r = false
+        } else {
+            sub1[val].style.display = 'none'
+            sub4r = true;
+        }
+    }
 }
--- Aplication_01/login.php
+++ Aplication_01/login.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @internal incluindo meu arquivo de conexão com o Banco:
  */
@@ -8,43 +9,89 @@ include_once("./php/connectedBD.php");
 
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <title>Título da página</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="short icon" href="./icones/Favicons/restricted_folder_file_10341 (1).ico">
-    <link rel="stylesheet" href="./css/style.css" type="text/css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Pangolin&display=swap" rel="stylesheet">
-    <script src="./js/file-01.js"></script>
-  </head>
-  <body id="corpo-1">
-    <main>
-      <article>
-      <div id="box-1"><h2 id="title-0"> BEM VINDO AO BANCO DO ANDRE </h2></div>
-         <div id="box-2"><h3 id="forms-1" name="titulo formulario"> FAÇA LOGIN NO SISTEMA </h3>
-         <br><br>              
-             <form method="POST" action=""> <!--Criado um Metodo de envio POST, sem action ainda-->
-                <p><input  placeholder="Email" type="text"  name="email"></p>
-                <br>
-                <p><input  placeholder="Password" type="password"  alt="alternate" name="senha"></p>
-                <br>
-                <!--Acima forma criados 02 Labels com 02 IDs -->
-                <div id=" senha01 "><p><a id="fogout-password" href="#"> Esqueceu sua senha</a></p></div>
-               
-                <br>
-                <span><input class="botao" type="button" value="Entrar"></span>
-                <span><input class="botao" type="button" value="Limpar"><span>
-                <br>
-                <!--Foi criado um Botão personalizado com efeito para enviar as informações -->
-                <!--Foi adicionado uma classe para atender todos os padrãoes -->
-            </form>
+
+<head>
+  <title>Título da página</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="short icon" href="./icones/Favicons/restricted_folder_file_10341 (1).ico">
+  <link rel="stylesheet" href="./css/style.css" type="text/css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Pangolin&display=swap" rel="stylesheet">
+  <script src="./js/file-01.js"></script>
+</head>
+
+<body id="corpo-1">
+  <main>
+    <article>
+      <div id="box-1">
+        <h2 id="title-0"> BEM VINDO AO BANCO DO ANDRE </h2>
+      </div>
+      <div id="box-2">
+        <h3 id="forms-1" name="titulo formulario"> FAÇA LOGIN NO SISTEMA </h3>
+        <br><br>
+        <form method="POST" action="">
+          <!--Criado um Metodo de envio POST, sem action ainda-->
+          <p><input placeholder="Email" type="text" name="email"></p>
+          <br>
+          <p><input placeholder="Password" type="password" alt="alternate" name="senha"></p>
+          <br>
+          <!--Acima forma criados 02 Labels com 02 IDs -->
+          <div id=" senha01 ">
+            <p><a id="fogout-password" href="#"> Esqueceu sua senha</a></p>
           </div>
-       </div>
-     </article>
-   </main>
-<footer>
-  
-</footer>          
-  </body>
+
+          <br>
+          <span><input class="botao" type="button" value="Entrar"></span>
+          <span><input class="botao" type="button" value="Limpar"><span>
+              <br>
+              <!--Foi criado um Botão personalizado com efeito para enviar as informações -->
+              <!--Foi adicionado uma classe para atender todos os padrãoes -->
+        </form>
+      </div>
+      </div>
+    </article>
+  </main>
+  <footer>
+    <div class="contents_footer">
+      <div class="content">
+        <p><a href="#" onclick="subcontent(0)">Conteúdo</a></p>
+        <div id="subcontent1" class="subcontent">
+          <p><a href="#">Sub Conteúdo</a></p>
+          <p><a href="#">Sub Conteúdo</a></p>
+          <p><a href="#">Sub Conteúdo</a></p>
+          <p><a href="#">Sub Conteúdo</a></p>
+        </div>
+      </div>
+      <div class="content">
+        <p><a href="#" onclick="subcontent(1)">Conteúdo</a></p>
+        <div id="subcontent2" class="subcontent">
+          <p><a href="#">Sub Conteúdo</a></p>
+          <p><a href="#">Sub Conteúdo</a></p>
+          <p><a href="#">Sub Conteúdo</a></p>
+          <p><a href="#">Sub Conteúdo</a></p>
+        </div>
+      </div>
+      <div id="subcontent3" class="content">
+        <p><a href="#" onclick="subcontent(2)">Conteúdo</a></p>
+        <div class="subcontent">
+          <p><a href="#">Sub Conteúdo</a></p>
+          <p><a href="#">Sub Conteúdo</a></p>
+          <p><a href="#">Sub Conteúdo</a></p>
+          <p><a href="#">Sub Conteúdo</a></p>
+        </div>
+      </div>
+      <div class="content">
+        <p><a href="#" onclick="subcontent(3)">Conteúdo</a></p>
+        <div id="subcontent4" class="subcontent">
+          <p><a href="#">Sub Conteúdo</a></p>
+          <p><a href="#">Sub Conteúdo</a></p>
+          <p><a href="#">Sub Conteúdo</a></p>
+          <p><a href="#">Sub Conteúdo</a></p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</body>
+
 </html>
